deefb61121acde84cd11bb20b47a4db494126e4b
聪头的游戏世界 <1322080797@qq.com>
2024-09-18 17:51:07 +0800

    自定义模型

--- custom/custom_editor_model_preview_icon.py
+++ custom/custom_editor_model_preview_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134220258: {'path': 'UP1ModelPreview/134220258', 'key': 134220258, 'size': '100X68', 'source_path': 'C:\\Users\\13220\\AppData\\Local\\Temp\\134220258.png'}, 134241327: {'path': 'UP1ModelPreview/134241327', 'key': 134241327, 'size': '100X68', 'source_path': 'C:\\Users\\13220\\AppData\\Local\\Temp\\134241327.png'}, 134260846: {'path': 'UP1ModelPreview/134260846', 'key': 134260846, 'size': '100X68', 'source_path': 'C:\\Users\\13220\\AppData\\Local\\Temp\\134260846.png'}, 134246438: {'path': 'UP1ModelPreview/134246438', 'key': 134246438, 'size': '100X68', 'source_path': 'C:\\Users\\13220\\AppData\\Local\\Temp\\134246438.png'}}
