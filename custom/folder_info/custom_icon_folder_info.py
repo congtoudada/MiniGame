8ebb06cd74f56060950c1361154f62b2fb40528d
聪头的游戏世界 <1322080797@qq.com>
2024-09-19 16:25:20 +0800

    导入图片

--- custom/folder_info/custom_icon_folder_info.py
+++ custom/folder_info/custom_icon_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {134220205: (2147483647, 0), 134243634: (2147483647, 1)}}
