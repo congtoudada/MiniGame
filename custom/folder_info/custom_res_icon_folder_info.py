36829acf5e7a172b9e14789686137cf6d0d5e851
聪头的游戏世界 <45703919+congtoudada@users.noreply.github.com>
2024-09-19 18:39:07 +0800

    TODO:数值

--- custom/folder_info/custom_res_icon_folder_info.py
+++ custom/folder_info/custom_res_icon_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {'ui/134220205': (2147483647, 0)}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {'ui/134220205': (2147483647, 0), 'ui/134243634': (2147483647, 1), 'ui/134240760': (2147483647, 2)}}
